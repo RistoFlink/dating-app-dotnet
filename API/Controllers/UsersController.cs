90761810a1a4f2980dcaf9f00222ecb03a145533
RistoFlink <kenssi@gmail.com>
2024-08-18 22:23:02 +0300

    Create a repository for Users, update the UsersController and add a DTO for Members

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,27 +1,26 @@
 using API.Data;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class UsersController(DataContext context) : BaseAPIController
+[Authorize]
+public class UsersController(IUserRepository userRepository) : BaseAPIController
 {
-    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        var users = await context.Users.ToListAsync();
-
+        var users = await userRepository.GetUsersAsync();
         return Ok(users);
     }
     
-    [Authorize]
-    [HttpGet("{id:int}")] // /api/users/id
-    public async Task<ActionResult<AppUser>> GetUser(int id) 
+    [HttpGet("{username}")] // /api/users/id
+    public async Task<ActionResult<AppUser>> GetUser(string username)
     {
-        var user = await context.Users.FindAsync(id);
+        var user = await userRepository.GetUserByUsernameAsync(username);
 
         if (user == null) return NotFound();
         
