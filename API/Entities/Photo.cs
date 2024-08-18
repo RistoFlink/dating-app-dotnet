1d81e4be74ea17e24fe4b42261d8031abad7826a
RistoFlink <kenssi@gmail.com>
2024-08-18 20:53:22 +0300

    Create seed data for the database

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities;
 
+[Table("Photos")]
 public class Photo
 {
     public int Id { get; set; }
@@ -8,5 +11,10 @@ public class Photo
     
     public bool IsMain { get; set; }
     
-    public string PublicId { get; set; }
+    public string? PublicId { get; set; }
+    
+    //navigation properties
+    public int AppUserId { get; set; }
+
+    public AppUser AppUser { get; set; } = null!;
 }
