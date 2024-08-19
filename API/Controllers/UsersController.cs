ab21d78c8493d021c508c4953283adbd1782ceb6
RistoFlink <kenssi@gmail.com>
2024-08-19 16:47:01 +0300

    Add and configure AutoMapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,19 +13,20 @@ namespace API.Controllers;
 public class UsersController(IUserRepository userRepository) : BaseAPIController
 {
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
     {
-        var users = await userRepository.GetUsersAsync();
+        var users = await userRepository.GetMembersAsync();
+        
         return Ok(users);
     }
     
     [HttpGet("{username}")] // /api/users/id
-    public async Task<ActionResult<AppUser>> GetUser(string username)
+    public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
-        var user = await userRepository.GetUserByUsernameAsync(username);
+        var user = await userRepository.GetMemberAsync(username);
 
         if (user == null) return NotFound();
-        
+
         return user;
     }
     
--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -4,7 +4,7 @@ public class MemberDto
 {
     public int Id { get; set; }
 
-    public string? UserName { get; set; }
+    public string? Username { get; set; }
     
     public int Age { get; set; }
     
