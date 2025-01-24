dff04c325f3de3281d070f66c3a4701372fc77f8
RistoFlink <kenssi@gmail.com>
2025-01-24 22:01:00 +0200

    Implement updating the user in the client app

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,10 +1,11 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, HostListener, inject, OnInit, ViewChild} from '@angular/core';
 import {Member} from "../../_models/member";
 import {AccountService} from "../../_services/account.service";
 import {MembersService} from "../../_services/members.service";
 import {RouterLink} from "@angular/router";
 import {TabsModule} from "ngx-bootstrap/tabs";
-import {FormsModule} from "@angular/forms";
+import {FormsModule, NgForm} from "@angular/forms";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-member-edit',
@@ -18,9 +19,16 @@ import {FormsModule} from "@angular/forms";
   styleUrl: './member-edit.component.css'
 })
 export class MemberEditComponent implements OnInit{
+  @ViewChild("editForm") editForm?: NgForm;
+  @HostListener("window:beforeunload", ["$event"]) notify($event: any) {
+    if (this.editForm?.dirty) {
+      $event.returnValue = true;
+    }
+  }
   member?: Member;
   private accountService = inject(AccountService);
   private memberService = inject(MembersService);
+  private toastr = inject(ToastrService);
 
   ngOnInit() {
     this.loadMember();
@@ -32,6 +40,15 @@ export class MemberEditComponent implements OnInit{
 
     this.memberService.getMember(user.username).subscribe({
       next: member => this.member = member
+    })
+  }
+
+  updateMember() {
+    this.memberService.updateMember(this.editForm?.value).subscribe({
+      next: _ => {
+        this.toastr.success("Profile updated successfully")
+        this.editForm?.reset(this.member);
+      }
     })
   }
 }
