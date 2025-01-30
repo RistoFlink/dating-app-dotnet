10365e946e75df0e6297fc415d9c7a8e60f338c9
RistoFlink <kenssi@gmail.com>
2025-01-30 21:50:47 +0200

    Add a phote uploader

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -6,6 +6,7 @@ import {RouterLink} from "@angular/router";
 import {TabsModule} from "ngx-bootstrap/tabs";
 import {FormsModule, NgForm} from "@angular/forms";
 import {ToastrService} from "ngx-toastr";
+import {PhotoEditorComponent} from "../photo-editor/photo-editor.component";
 
 @Component({
   selector: 'app-member-edit',
@@ -13,7 +14,8 @@ import {ToastrService} from "ngx-toastr";
   imports: [
     RouterLink,
     TabsModule,
-    FormsModule
+    FormsModule,
+    PhotoEditorComponent
   ],
   templateUrl: './member-edit.component.html',
   styleUrl: './member-edit.component.css'
@@ -50,5 +52,9 @@ export class MemberEditComponent implements OnInit{
         this.editForm?.reset(this.member);
       }
     })
+  }
+
+  onMemberChange(event: Member) {
+    this.member = event;
   }
 }
